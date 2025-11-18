efc4c5d1bef4c160689be67dd174a3b1e5050f4a
Tsuyakashi <kashakdaniil@gmail.com>
2025-11-19 01:25:32 +0300

    ans & sh fixes

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # aws-vpn
+## Usage
+- clone repo
+- run quistart script
+## Needs:
+- terraform
+- aws cli
+- ansible core
 ## To-do list:
+- write ansible autconfiguration
 - rewrite creating keys with [hashicorp/tls](https://dev.to/bansikah/deploying-an-aws-ec2-instance-with-terraform-and-ssh-access-d09#:~:text=your%20local%20machine:-,Terraform,key%20parameters%20for%20the%20deployment)
 - rewrite .tf as modules
--- quickstart.sh
+++ quickstart.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+set -e
+# terraform apply DONT WORK, run urself 
 echo "appling terraform"
-cd terraform && terraform apply --auto-approve
-tee -a ../ansible/inventory.ini > /dev/null <<EOF
-['hosts]
+cd terraform
+# terraform init
+# terraform apply --auto-approve
+
+echo "creating inventory for ansible"
+tee -a ../ansible/hosts > /dev/null <<EOF
+[hosts]
 $(echo $(terraform output) | awk -F'"' '{print $2}')
 EOF
 
+# also dont work yet :D
+
 echo "starting ansible"
 cd ../ansible
-# ansible myhosts -m ping -i inventory.ini
+ansible all -i hosts -i inventory.ini -m ping 
+# ansible-playbook -i hosts -i inventory.ini main.yml
