bf86ba1a2f7db4fa8f48dcd9432c79305531e263
Tsuyakashi <kashakdaniil@gmail.com>
2025-11-19 22:45:30 +0300

    rm outdated

--- quickstart.sh
+++ quickstart.sh
@@ -23,17 +23,6 @@ tee -a ../ansible/hosts > /dev/null <<EOF
 $(echo $(terraform output) | awk -F'"' '{print $2}')
 EOF
 
-# while true; do
-#     if [[ "$(terraform output -raw instance_state 2>/dev/null)" == "running" ]]; then
-#         echo "Instance running"
-#         break
-#     fi
-
-#     echo "Instance didnt start yet"
-#     sleep 10
-# done
-# что-то нерабочее
-
 while ! nc -z "$(terraform output -raw instance_hostname)" 22 >/dev/null 2>&1; do
     echo "Instance didnt start yet"
     sleep 5
