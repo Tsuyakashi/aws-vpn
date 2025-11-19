47408d56d05f6170641c53c044e02c3fdbb5edd2
Tsuyakashi <kashakdaniil@gmail.com>
2025-11-19 22:14:11 +0300

    write ansible autconfiguration

--- README.md
+++ README.md
@@ -7,6 +7,5 @@
 - aws cli
 - ansible core
 ## To-do list:
-- write ansible autconfiguration
 - rewrite creating keys with [hashicorp/tls](https://dev.to/bansikah/deploying-an-aws-ec2-instance-with-terraform-and-ssh-access-d09#:~:text=your%20local%20machine:-,Terraform,key%20parameters%20for%20the%20deployment)
 - rewrite .tf as modules
--- quickstart.sh
+++ quickstart.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 set -e
-# terraform apply DONT WORK, run urself 
+
 echo "appling terraform"
 cd terraform
-# terraform init
-# terraform apply --auto-approve
+terraform init
+terraform apply --auto-approve
 
 echo "creating inventory for ansible"
+if [ -f "../ansible/hosts" ]; then
+    rm ../ansible/hosts
+fi
+
 tee -a ../ansible/hosts > /dev/null <<EOF
 [hosts]
 $(echo $(terraform output) | awk -F'"' '{print $2}')
 EOF
 
-# also dont work yet :D
+# while true; do
+#     if [[ "$(terraform output -raw instance_state 2>/dev/null)" == "running" ]]; then
+#         echo "Instance running"
+#         break
+#     fi
+
+#     echo "Instance didnt start yet"
+#     sleep 10
+# done
+# что-то нерабочее
+
+while ! nc -z "$(terraform output -raw instance_hostname)" 22 >/dev/null 2>&1; do
+    echo "Instance didnt start yet"
+    sleep 5
+done
+
 
 echo "starting ansible"
 cd ../ansible
-ansible all -i hosts -i inventory.ini -m ping 
-# ansible-playbook -i hosts -i inventory.ini main.yml
+ansible-playbook -i hosts -i inventory.ini main.yml
